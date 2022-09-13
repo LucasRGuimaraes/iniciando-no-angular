f734c24f7921c31398b9c813a3969d2ac0665c16
Lucas Guimarães <89198796+LucasRGuimaraes@users.noreply.github.com>
2022-09-13 09:53:58 -0300

    Adicionado Service de Cursos

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,21 +1,19 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
 })
 export class CursosComponent implements OnInit {
-
   nomePortal: string = '';
+  cursos: string[];
 
-  cursos: string[] = ['Java', 'React', 'Angular']
-
-  constructor() {
-    this.nomePortal = 'https://lucasguimaraes.com.br'
-   }
-
-  ngOnInit(): void {
+  constructor(private cursoService: CursosService) {
+    this.nomePortal = 'https://lucasguimaraes.com.br';
+    this.cursos = this.cursoService.getCursos() 
   }
 
+  ngOnInit(): void {}
 }
